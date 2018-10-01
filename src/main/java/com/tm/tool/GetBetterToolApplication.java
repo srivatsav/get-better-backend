c47f3bb889d12b06a062fd48c9880e294eac5100
srivatsav <srivatsav@192.168.0.6>
2018-10-01 21:31:37 +0530

    Removing Application.java - duplicate config file

--- src/main/java/com/tm/tool/GetBetterToolApplication.java
+++ src/main/java/com/tm/tool/GetBetterToolApplication.java
@@ -2,8 +2,10 @@ package com.tm.tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.tm.*")
 public class GetBetterToolApplication {
 
 	public static void main(String[] args) {
